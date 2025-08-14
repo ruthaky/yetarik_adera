dc1d75554888dbc155d1c8143a122dc75f3ab1cb
Ruth Asmamaw <ruthasmamaw@192.168.1.3>
2025-08-14 16:40:38 +0300

    footer

--- app/shared/footer/footer.tsx
+++ app/shared/footer/footer.tsx
@@ -20,35 +20,37 @@ export default function Footer() {
   };
   return (
     <div>
-      <footer className="bg-[#2b2a28] text-white px-6 py-10 text-sm">
+      <footer className="bg-[#333333] text-white px-14 py-16 text-sm">
         <div className="flex flex-col md:flex-row justify-between">
           <div>
-            <h4 className="font-bold mb-1">YETARIK ADERA</h4>
-            <p>Contact us: info@yetarikadera.org</p>
-            <div className="flex space-x-2 mt-2">
+            <h4 className="font-bold text-[25px] mb-1">YETARIK ADERA</h4>
+            <p className="py-2">Contact us: info@yetarikadera.org</p>
+            <div className="flex space-x-6 mt-2">
               <a href="#">Instagram</a>
               <a href="#">YouTube</a>
               <a href="#">TikTok</a>
-              <a href="#">Medium</a>
+              <a href="#">Facebook</a>
             </div>
           </div>
           <div className="mt-4 md:mt-0">
-            <p className="mb-1">Get the freshest news from us:</p>
-            <div className="flex">
+            <p className="mb-1 py-2">Get the freshest news from us:</p>
+            <div className="flex gap-2">
               <input
                 type="email"
                 placeholder="Your email address"
-                className="px-2 py-1 text-black"
+                className="px-2 py-1 text-black rounded-[4px] w-[250px]"
               />
-              <button className="bg-orange-400 text-black px-3 py-1">
+              <button className="bg-[#B8B0BD] text-white px-4 py-2 rounded-[4px]">
                 Subscribe
               </button>
             </div>
           </div>
         </div>
-        <div className="text-xs mt-4 flex justify-between">
-          <p>Terms • Privacy • Accessibility • Legal</p>
-          <p>Design with love © yetarikadera 2024</p>
+        <div className="h-[1px] w-full bg-[#71777D] my-8"></div>
+        <div className="text-xs mt-4 flex justify-between text-[#92989F]">
+          <div className="flex gap-4 "><div>Terms & Conditions</div><div>|</div><div>Privacy Policy</div><div>|</div><div>Accessibility</div><div>|</div><div>Legal</div></div>
+          
+          <p>Design with love © yetarikadera 2024. All rights reserved</p>
         </div>
       </footer>
     </div>
